3294b4b4f228c34e74b4d9786ec18eb9b224458c
Georgii Naumov <gmnaumov@yandex.ru>
2024-01-04 20:32:50 +0300

    feat:Added localstorage handling

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,35 @@
-var myImage = document.querySelector("img");
+let myImage = document.querySelector("img");
 
 myImage.onclick = function() {
-    var mySrc = myImage.getAttribute("src");
+    let mySrc = myImage.getAttribute("src");
     if (mySrc === "images/thunderbird-logo.png") {
         myImage.setAttribute("src", "images/mozilla-logo.jpg");
     } else {
         myImage.setAttribute("src", "images/thunderbird-logo.png");
     }
 }
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    let myName = prompt("Please, enter your name: ");
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = "Mozilla is cool, " + myName;
+    }
+}
+
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem("name");
+    myHeading.innerHTML = "Mozilla is cool, " + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+};
